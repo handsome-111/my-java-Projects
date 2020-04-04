6a85b58f12cfa344ba9f6678525115577c5c5d14
1040978586@qq.com <1040978586@qq.com>
2020-04-04 20:46:19 +0800

    1

---
+++ spring-cloud-alibaba-demo/springcloud-alibaba-nacos-demo01/src/main/java/com/study/springboottesting/SpringcloudAlibabaNacosDemo01Application.java
@@ -0,0 +1,13 @@
+package com.study.springboottesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudAlibabaNacosDemo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudAlibabaNacosDemo01Application.class, args);
+	}
+
+}
